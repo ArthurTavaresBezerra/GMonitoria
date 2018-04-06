fb654a94e34555b0e708565de81c39a36ab67033
Arthur <arthur@shoply.com.br>
2018-04-06 15:00:23 -0300

    Arquivos Faltando

---
+++ GMonitoria.Domain/Entities/ProcessoSeletivo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GMonitoria.Domain.Entities
+{
+    public partial class ProcessoSeletivo
+    {
+        public ProcessoSeletivo()
+        {
+            ProcessoSeletivoCurso = new HashSet<ProcessoSeletivoCurso>();
+        }
+
+        public string ProcessoSeletivoId { get; set; }
+        public string Periodo { get; set; }
+        public DateTime Datahora { get; set; }
+        public bool IsIniciado { get; set; }
+        public bool? IsConcluido { get; set; }
+
+        public ICollection<ProcessoSeletivoCurso> ProcessoSeletivoCurso { get; set; }
+    }
+}
