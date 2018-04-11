c5420b7216700e7c7374ccf2075e24e74b6db1a1
Victor Neri <victorneri@MacBook-Pro-de-Victor.local>
2018-04-11 19:49:49 -0300

    Adicionando o Material Designer

--- GMonitoriaApp/src/app/app.module.ts
+++ GMonitoriaApp/src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,BrowserAnimationsModule,NoopAnimationsModule,MatButtonModule,MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
