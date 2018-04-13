68a4906dbc771fe4155ef0370bd27a0cd8503154
Victor Neri <victorneri@MacBook-Pro-de-Victor.local>
2018-04-13 20:39:56 -0300

    App Module estruturada com materal Module, BoostrapModule e outros

--- GMonitoriaApp/src/app/app.module.ts
+++ GMonitoriaApp/src/app/app.module.ts
@@ -1,45 +1,126 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
+import {CdkTableModule} from '@angular/cdk/table';
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'; 
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+/** Materal Angular */
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+} from '@angular/material';
+
+@NgModule({
+  exports: [
+    CdkTableModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+  ]
+})
+export class MaterialModule {}
+
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCheckboxModule, MatFormField, MatSelect, MatOption } from '@angular/material';
-import { LoginComponent } from './login/login.component';
-import { MatInputModule } from '@angular/material/input';
-
+/**ngx-bootStrap */
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
- 
+
+@NgModule({ exports: [BsDropdownModule, TooltipModule, ModalModule]})  export class NgxBootstrapModule {}
+/** MdbBoots */
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
   
-import { PageMainStudentComponent } from './page-main-student/page-main-student.component';
 
 import {RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
 import { PageMainCollaboratorComponent } from './page-main-collaborator/page-main-collaborator.component';
+import { PageMainStudentComponent } from './page-main-student/page-main-student.component';
+import { LoginComponent } from './login/login.component';
+
 
 const appRoutes: Routes = [
+  { path: '',
+    component: LoginComponent,
+    pathMatch: 'full'
+  },
   {
     path: 'page',
     component: PageMainStudentComponent,
     data: { title: 'Heroes List' }
   },{
     path: 'collaborator',
-    component: PageMainCollaboratorComponent,
-    
-  },
-  { path: '',
-    component: LoginComponent,
-    pathMatch: 'full'
+    component: PageMainCollaboratorComponent
   },
   { path: '**', component: LoginComponent }
 ];
 
 @NgModule({
-  declarations: [ 
+  declarations: [  
     AppComponent,
     LoginComponent,
     PageMainStudentComponent,
@@ -47,12 +128,18 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule,
+    HttpClientModule,
+    MaterialModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    NoopAnimationsModule,
+    NgxBootstrapModule,
     RouterModule.forRoot(appRoutes,{ enableTracing: true }), // <-- debugging purposes only
-    //Material Design
-    BrowserAnimationsModule,NoopAnimationsModule,MatButtonModule,MatCheckboxModule,MatInputModule,
-    //BootsTrap Js Actions
-    BsDropdownModule.forRoot(),TooltipModule.forRoot(),ModalModule.forRoot(),MDBBootstrapModule.forRoot()
+    
+    //BsDropdownModule.forRoot(),TooltipModule.forRoot(),ModalModule.forRoot(),MDBBootstrapModule.forRoot()
 
 
   ],
@@ -60,3 +147,5 @@ const appRoutes: Routes = [
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+ 
+platformBrowserDynamic().bootstrapModule(AppModule);
